a310608f668ac79f1700121dc70df19461d013ea
momi329 <114843634+momi329@users.noreply.github.com>
2023-04-07 14:33:28 +0800

    Update Firebaseapp.js

--- src/Firebaseapp.js
+++ src/Firebaseapp.js
@@ -7,13 +7,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDjoZe3affRBuw-DUZ5WwCtBXVxc4oi0BI",
-  authDomain: "tarot-diary.firebaseapp.com",
-  projectId: "tarot-diary",
-  storageBucket: "tarot-diary.appspot.com",
-  messagingSenderId: "339110698426",
-  appId: "1:339110698426:web:f115d61ea8668a6a565183",
-  measurementId: "G-L8RC14KPQY",
+
 };
 
 // Initialize Firebase
